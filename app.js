8953f56bf33b5f453f4fbb49fcab809184e1c6bd
Gbadamosi Mubarak <gbadamosimubarak60@gmail.com>
2022-04-08 04:28:08 -0700

    changing the files

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-$(document).ready(function(){
+document.ready(function(){
 
-  $('#searchbar').focus();
+  document.getElementById("searchbar").focus();
 
-  $('#donate-buttons').on('click', '.btn-blue', function(e) {
-    e.preventDefault(); 
-    $('.active').removeClass('active');
-    $('#other-input').hide().siblings('#other').show();
-    $(this).filter('.btn-blue').addClass("active");
-    var value = $(this).data('impact');
-    $(this).closest('div').find('p').text("" + value);
-    $('#other-input').find('input').val('');  
+  document.getElementById("donate-buttons").on('click', '.btn-blue', function(e) {
+    e.preventDefault();
+    document.getElementByClassName("active").classList.remove("active");
+    document.getElementById("other-input").style.display = "none".siblings('#other').style.display = "";
+    this.filter('.btn-blue').classList.add("active");
+    let value = this.data('impact');
+    this.closest('div').find('p').innerText = "" + value;
+    document.getElementById("other-input").find('input').value = '';  
   });
     
-  $('.btn-green').on('click', function() {
-    var dollar;
-    var input = $('#other-input').find('input').val();
+  document.getElementByClassName("btn-green").addEventListener('click', (e) => {
+    let dollar;
+    let input = document.getElementById("other-input").find('input').value;
     if ( !input ) {
-      dollar = $('.active').data('dollars');
-     } else if ( $.trim(input) === '' || isNaN(input)) {
-      // empty space leaves value = 'undefined'. 
-      // Have to fix $.trim(input) == '' above so that it works.
+      dollar = document.getElementByClassName("active").data('dollars');
+     } else if ( document.querySelector.trim(input) === '' || isNaN(input)) {
+      
+      
       console.log('Yes');
       dollar = "Please enter a number."; 
     } else {
       dollar = input;
     }
-    $('#price').text(""+dollar);
+    document.getElementById("price").innerText = ""+dollar;
   });
 
-  $('#other').on('click', function(e) {
+  document.getElementById("other").addEventListener('click', (e) => {
     e.preventDefault(); 
-    var buttons = $(this).parent('#donate-buttons');
-    buttons.find('.active').removeClass('active');
-    var other = $(this).hide().siblings('#other-input');
-    other.show();
+    let buttons = this.parent('#donate-buttons');
+    buttons.find('.active').classList.remove("active");
+    let other = this.style.display = "none".siblings('#other-input');
+    other.style.display = "";
     other.find('input').focus();
-    var pText = buttons.siblings('p');
-    pText.text("Thank you!");
-    var oValue = other.find('input');
+    let pText = buttons.siblings('p');
+    pText.innerText = "Thank you!";
+    let oValue = other.find('input');
     oValue.keyup(function() {
-      if ( oValue.val() > 50 ) { 
-        pText.text("Thank you!" + " You\'re donation covers housing and counseling services for " + oValue.val()/25 + " people.");
+      if ( oValue.value > 50 ) {
+        pText.innerText = "Thank you!" + " You're donation covers housing and counseling services for " + oValue.val/(25 + " people.");
       } else {
-        pText.text("Thank you!");
+        pText.innerText = "Thank you!";
       }
     });
   }); 
